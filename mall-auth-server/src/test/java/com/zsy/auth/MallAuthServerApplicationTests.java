e1b09b6eb2f733f7a840e1d132c278758f6c0623
yukinocyann <yukinoyannA@gmail.com>
2021-09-05 10:24:33 +0800

    测试

--- mall-auth-server/src/test/java/com/zsy/auth/MallAuthServerApplicationTests.java
+++ mall-auth-server/src/test/java/com/zsy/auth/MallAuthServerApplicationTests.java
@@ -9,5 +9,24 @@ class MallAuthServerApplicationTests {
     @Test
     void contextLoads() {
     }
+    static class Shape{
+        Shape(int i){
+            System.out.println("This is Shape" + i);
+        }
+    }
+
+    public static class Circle extends Shape{
+        static Shape s1 = new Shape(1);
+        Shape s2 = new Shape(3);
+        Circle(int i){
+            super(i);
+            System.out.println("This is Circle" + i);
+        }
+        @Test
+        public static void main(String args[]){
+            Circle c1 = new Circle(2);
+        }
+    }
+
 
 }
